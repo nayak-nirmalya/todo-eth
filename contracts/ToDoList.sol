6e6c58259cebbda3fb6a8c21fab9ac1dca622256
Nirmalya Nayak <n.nayak263@gmail.com>
2022-05-12 07:33:00 +0530

    Frontend Stuff

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -3,4 +3,23 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract ToDoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() {
+        createTask("Demo Task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
+
+//const toDoList = await ToDoList.deployed()
